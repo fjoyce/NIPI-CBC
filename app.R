b9e40d5405f91b5c793383c87bdcb56386c4fa27
Francis Joyce <fjoyce@ucsc.edu>
2020-12-28 17:22:37 -0600

    add annotations & leaflet map, change encoding to uft-8

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(leaflet)
 library(dplyr)
 library(ggplot2)
 library(readr)
@@ -36,11 +37,12 @@ years_list <- NIPI %>%
 year_min_max <- min_max(years_list)
 
 
-ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
+#navbarPage is a shiny page layout with a nav bar on top. Title for overall app.
+ui <- navbarPage("Conteo Navideño de Aves- Paso del Istmo 2015-2020",
                  
                  tabPanel(
                      
-                     # App title ----
+                     # First Tab title ----
                      titlePanel(tags$h4("Tendencias por especie")),
                      
                      # Sidebar layout with input and output definitions ----
@@ -51,7 +53,7 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                              
                             #once I figure out how to toggle betwen scientific and common names this is the control
                              #radioButtons("radio", label = h3("Especies"),
-                                        #choices = list("científicos" = "species_latin", "inglés" = "species"), 
+                                        #choices = list("cientÃ­ficos" = "species_latin", "inglÃ©s" = "species"), 
                                         #selected = 1),
                            
                              tags$h3("Especies"),
@@ -59,7 +61,7 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                              # Input: which species ----
                              selectizeInput("species_picked",
                                             multiple = TRUE,
-                                            selected = c("Quiscalus mexicanus", "Amazona auropalliata", "Brotogeris jugularis", "Eupsittula canicularis", "Amazona albifrons"),
+                                            selected = c("Eumomota superciliosa", "Amazona auropalliata", "Brotogeris jugularis", "Eupsittula canicularis", "Amazona albifrons", "Amazona autumnalis"),
                                             label = "Puede seleccionar los nombres científicos de la lista o escribirlos (máximo 6):",
                                             choices = species_list,
                                             options = list(maxItems = 6)),
@@ -75,15 +77,16 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                                          value = c(2015, year_min_max[2])),
                              
                              helpText(tags$ul(
-                                 tags$li("Tenga en cuenta que los ejes verticales no siempre empiezan en 0"),
+                                 tags$li("Tenga en cuenta que los ejes verticales tienen escalas diferentes"),
                                  
-                                 #tags$li("Los datos no incluyen aves avistados únicamente durante los tres días antes o después del conteo (semana de conteo)"),
+                                 #tags$li("Los datos no incluyen aves avistados Ãºnicamente durante los tres dÃ­as antes o despuÃ©s del conteo (semana de conteo)"),
                                  
-                                 #tags$li("El conteo se ha realizao todos los años el 28 de diciembre"),
+                                 tags$li("El conteo se ha realizado durante la primara semana de enero"),
                                  
                                  #tags$li("No incluye aves no identificadas a nivel de especie"),
                                  
-                                 tags$li("Por cambios en taxonomía y nomenclatura algunas especies aparecen bajo nombres científicos desactualizados")
+                                 tags$li("Por cambios en taxonomía y nomenclatura algunas especies aparecen bajo nombres científicos desactualizados"),
+                                 tags$li("Los años en esta página corresponden a la fecha el conteo, no al año de conteo utilizado por Audubon")
                              
                     
                              )
@@ -102,10 +105,10 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                      )
                  ),
                  
-                 
+                 #Second tab for species ranked by detections by year
                  tabPanel(
                      
-                     titlePanel(tags$h4("Especies con mayor detecciones por año")),
+                     titlePanel(tags$h4("Especies más detectadas por año")),
                      
                      # Sidebar layout with input and output definitions ----
                      sidebarLayout(
@@ -115,7 +118,7 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                              
                              # Input: which year ----
                              selectInput("individual_year_picked",
-                                         label = "¿Para cuál año quisiera visualizar los resultados del Conteo Navideño de Aves Paso del Istmo?",
+                                         label = "¿Para cuál año quiere visualizar los resultados del Conteo Navideño de Aves Paso del Istmo?",
                                          choices = years_list),
                              
                              
@@ -134,8 +137,26 @@ ui <- navbarPage("Conteo Navideño de Aves Paso del Istmo 2015-2020",
                      
                      
                      
+                 ),
+                 
+                 #Third panel for circle map
+                 tabPanel(
+                     
+                     titlePanel(tags$h4("Mapa del círculo")),
+                     
+                     #in theory no sidebar required?
+                     # Main panel for displaying outputs ----
+                     mainPanel(
+                         
+                         leafletOutput("mapa"),
+                         p()
+                         
+                     )
                  )
-)
+                     
+                     
+                     
+                 )
 
 # Define server logic required to draw a plot and table ----
 server <- function(input, output) {
@@ -162,7 +183,7 @@ server <- function(input, output) {
                 geom_line(size = 1) +
                 geom_point(size = 2) +
                 xlab("Año") +
-                ylab("Inividuos por hora") + #can change to "número/hora"
+                ylab("Inividuos por hora") + #can change to "nÃºmero/hora"
                 theme_minimal_hgrid()+
                 theme(text = element_text(size = 18),
                       legend.position = "none",
@@ -174,6 +195,7 @@ server <- function(input, output) {
                 #scale_colour_paletteer_c("tanagr::tangara_chilensis") +
                 #scale_color_manual(values = cal_palette("sierra1")) +
                 scale_x_continuous(breaks = seq(2015, 2020, by = 1)) +
+                expand_limits(y=0) +
                 scale_y_continuous(labels = comma)#+
                 #scale_color_tanagr(palette_name = "tangara_chilensis")
             
@@ -201,6 +223,22 @@ server <- function(input, output) {
             rename(Especie = species_latin, `Individuos contados` = how_many_counted)
         
     },
+    
+    # Third navbar output ----
+    output$mapa <- renderLeaflet({
+        leaflet() %>% 
+            addProviderTiles("Esri.WorldImagery") %>% 
+            addCircles(lng = -85.73,
+                       lat = 11.21,
+                       weight = 4,
+                       radius = 12070,
+                       color = "green",
+                       fillOpacity = 0,
+                       popup = "circulo de conteo") %>% 
+            setView(lat = 11.21,
+                    lng = -85.73,
+                    zoom = 11)
+    }),
     
     align = "lr")
 }
